a97bd7728cb288372dd69e3f6c7bee43b66310bf
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-02-19 09:03:27 +0700

    installig graphql

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -1,7 +1,29 @@
 import FeaturedItem from "./FeaturedItem";
 import data from "@/data/data";
+import { GraphQLClient, gql } from "graphql-request";
 
-export default function Featured() {
+const hygraph = new GraphQLClient(
+  "https://api-ap-southeast-2.hygraph.com/v2/cldwhj75u2fmv01rr5cw5gdw4/master"
+);
+
+const QUERY = gql`
+  query Works {
+    works {
+      id
+      title
+      slug
+      coverPhoto {
+        id
+        url
+      }
+    }
+  }
+`;
+
+export default async function Featured() {
+
+  const { works } = await hygraph.request(QUERY)
+
   return (
     <section className="rounded-t-3xl md:rounded-t-[5rem] overflow-hidden">
       <div>
