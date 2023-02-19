55bc20b43e8d83388e78e991374d47ebcb4a11cd
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-02-19 10:37:48 +0700

    adding footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,26 @@
+import { Twitter, Github, Instagram } from "lucide-react"
+
 export default function Footer() {
     return (
-        <footer className="bg-main text-white h-96">
-            ini footer
+        <footer className="bg-main flex flex-col gap-6 text-white px-4 md:px-8 py-10">
+            <div>
+                <h3 className="text-lg md:text-2xl">Don't be a stranger!</h3>
+                <p className="text-sm opacity-50">See me online at:</p>
+            </div>
+            <div className="flex gap-4">
+                <a href="https://github.com/wllyvx" target="_blank">
+                    <Github className="opacity-50 hover:opacity-100 cursor-pointer"/>
+                </a>
+                <a href="https://twitter.com/willyframadhan" target="_blank">
+                    <Twitter className="opacity-50 hover:opacity-100 cursor-pointer"/>
+                </a>
+                <a href="https://instagram.com/willyframadhan" target="_blank">
+                    <Instagram className="opacity-50 hover:opacity-100 cursor-pointer"/>
+                </a>
+            </div>
+            <div className="w-full flex justify-center">
+                <p className="text-sm">Made with ❣️ by Willy</p>
+            </div>
         </footer>
     )
 }
